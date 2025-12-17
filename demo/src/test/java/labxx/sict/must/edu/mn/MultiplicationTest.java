b53fe17a446bda1cce9d4200cff8ffb10fa850b7
Enkhbayar Ganbold <enkhbayarganbold@Enkhbayars-MacBook-Pro.local>
2025-12-17 15:45:17 +0800

    ci: test

--- demo/src/test/java/labxx/sict/must/edu/mn/MultiplicationTest.java
+++ demo/src/test/java/labxx/sict/must/edu/mn/MultiplicationTest.java
@@ -13,7 +13,7 @@ public class MultiplicationTest {
     @Test
     void testMultiplyPositive() {
         Multiplication calc = new Multiplication();
-        assertEquals(6.0, calc.multiply(2.0, 3.0), "2 * 3 should equal 6");
+        assertEquals(6.0 , calc.multiply(2.0, 3.0), "2 * 3 should equal 6");
     }
 
     /**
